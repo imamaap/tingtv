d270e3fb8ef7f88fa9b75a34e00080b80189facd
imamaap@gmail.com <imamaap@gmail.com@05a1ff3e-222d-de35-3783-ade064285d4c>
2014-11-03 22:06:29 +0000

    klik

--- trunk/fungsitingtv.js
+++ trunk/fungsitingtv.js
@@ -22,35 +22,3 @@ function changeIframeSrc(id, url) {
 }
 // ]]>
 
-function keypressed() 
-{
-alert("Mouse right click is disabled! ::miruTV.com::");
-}
-
-document.onkeydown=keypressed;
-
-//norightclick
-function shake() 
-{
-   if (event.button==2) {
-   var x=10
-   if (document.all||document.layers) 
-   {
-   for (i=0;i,i<20;i++){
-	window.moveBy(0,x)
-	window.moveBy(x,0)
-	window.moveBy(0,-x)
-	window.moveBy(-x,0)
-     	}
-    }
-  }
-}
-
-document.onmousedown=shake
-document.write('<body oncontextmenu="return false">');
-
-var message = "Mouse right click is disabled! ::miruTV.com:: ;D"; 
-function rtclickcheck(keyp){ if (navigator.appName == "Netscape" && keyp.which == 3){ 	alert(message); return false; } 
-
-if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) { 	alert(message); 	return false; } } 
-document.onmousedown = rtclickcheck;
